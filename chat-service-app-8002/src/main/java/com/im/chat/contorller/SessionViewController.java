a993924152e1bc63424f92f2969c1447cbd7f811
zhaomanzhou <zmzsstreet@163.com>
2020-08-21 15:21:30 +0800

    添加说明

--- chat-service-app-8002/src/main/java/com/im/chat/contorller/SessionViewController.java
+++ chat-service-app-8002/src/main/java/com/im/chat/contorller/SessionViewController.java
@@ -1,13 +1,16 @@
 package com.im.chat.contorller;
 
 
+import com.im.chat.annotation.CurrentUser;
+import com.mr.entity.po.User;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/chat")
 public class SessionViewController
 {
 
-    public void addSessionView()
+    //entityType --> U单聊  G群聊
+    public void addSessionView(@CurrentUser User user, String entityType, Long entityId)
     {
 
     }
